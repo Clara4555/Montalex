95760b9c8dad7f1bb395803fce537984e9b1bc3f
Clara4555 <favourloyalty8@gmail.com>
2025-03-08 03:50:42 -1200

    done with every remaining home and bunkering

--- src/script/navbar.js
+++ src/script/navbar.js
@@ -5,3 +5,26 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+
+function showTab(event, tabId) {
+    // Remove active class from all tabs
+    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.tab-pane').forEach(content => content.classList.remove('active'));
+
+    // Add active class to the clicked tab and corresponding content
+    event.currentTarget.classList.add('active');
+    document.getElementById(tabId).classList.add('active');
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectTitles = document.querySelectorAll(".project-title");
+
+    projectTitles.forEach(title => {
+        title.addEventListener("click", function () {
+            const parent = this.parentElement;
+            parent.classList.toggle("active");
+        });
+    });
+});
